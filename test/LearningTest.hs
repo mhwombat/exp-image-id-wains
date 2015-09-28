b95aaf6764aca00c47edd318c38aef3f975fab0c
Amy de Buitléir <amy@nualeargais.ie>
2015-09-28 18:06:44 +0100

    upgrade to new version of creatur-wains

--- test/LearningTest.hs
+++ test/LearningTest.hs
@@ -55,7 +55,7 @@ testWain :: ImageWain
 testWain = w'
   where wName = "Fred"
         wAppearance = bigX 28 28
-        wBrain = makeBrain wClassifier wMuser wPredictor wHappinessWeights
+        wBrain = makeBrain wClassifier wMuser wPredictor wHappinessWeights 1
         wDevotion = 0.1
         wAgeOfMaturity = 100
         wPassionDelta = 0
